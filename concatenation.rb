6a033e5ef6a40749dcd36491f25a7c0c72273e3b
Rakoski <mastrakoski@gmail.com>
2023-08-24 17:56:20 -0300

    concatenation.rb and escape_characters.rb

---
+++ concatenation.rb
@@ -0,0 +1,18 @@
+class Concatenation
+  # With the plus operator:
+  "Welcome " + "to " + "Odin!"    #=> "Welcome to Odin!"
+
+  # With the shovel operator:
+  "Welcome " << "to " << "Odin!"  #=> "Welcome to Odin!"
+
+  # With the concat method:
+  "Welcome ".concat("to ").concat("Odin!")  #=> "Welcome to Odin!"
+
+  "hello"[0]      #=> "h"
+
+  "hello"[0..1]   #=> "he"
+
+  "hello"[0, 4]   #=> "hell"
+
+  "hello"[-1]     #=> "o"
+end
---
+++ escape_characters.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class EscapeCharacters
+
+  print("Back\\slash ")
+  print("Back\bspace ")
+  print("new\nline ")
+  print("spa\sce ")
+  print("Ta\tb ")
+
+  print('Single quotation mark (escape characters \n dont work here)')
+
+  print("\n")
+  nome = "mateus"
+  print("Hello #{nome}")
+
+end
